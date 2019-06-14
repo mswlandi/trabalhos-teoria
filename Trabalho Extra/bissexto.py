36fd2c47249f249bc705ed468c54eb5a46e706d0
Frederico Messa Schwartzhaupt <frederico.messa@gmail.com>
2019-06-14 13:12:51 -0300

    Questão 1 – Trabalho Extra (melhorada)

--- Trabalho Extra/bissexto.py
+++ Trabalho Extra/bissexto.py
@@ -1,11 +1,12 @@
 file = open('bissexto.mn', 'w')
-file.write('main{\n')    
-for i in range(1581):
-    file.write('{0}: do dec X goto {1}\n'.format(i+1, i+2))
-file.write('{0}: if zero X then goto {1} else goto {2}\n'.format(1582, 0, 1583))
-file.write('{0}: do dec X goto {1}\n'.format(1583, 1584))
-k = 1584
+file.write('main{\n')
+file.write('1: do sub X 1581 goto 2\n')
+file.write('// n < 1582\n')
+file.write('2: if zero X then goto 0 else goto 3\n')
+file.write('3: do dec X goto 4\n')
+k = 4
 for i in range(400):
+    file.write('// n % 400 == {0}, n % 100 == {1}, n % 4 == {2}\n'.format((i + 1582) % 400, (i + 1582) % 100, (i + 1582) % 4))
     if (i + 1582) % 400 == 0:
         file.write('{0}: if zero X then goto {1} else goto {2}\n'.format(k+i, k+i+1, k+i+2))
         file.write('{0}: do inc Y goto {1}\n'.format(k+i+1, 0))
@@ -20,6 +21,6 @@ for i in range(400):
     else:
         file.write('{0}: if zero X then goto {1} else goto {2}\n'.format(k+i, 0, k+i+1))
         k += 1
-    file.write('{0}: do dec X goto {1}\n'.format(k+i, 1584 if i == 399 else k+i+1))
-file.write('}')    
+    file.write('{0}: do dec X goto {1}\n'.format(k+i, 4 if i == 399 else k+i+1))
+file.write('}')
 file.close()
